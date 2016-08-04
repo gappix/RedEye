fafc69c6c21a68a38220b00cfcee340fabaff7e0
gappix <paolo.gazzott@gmail.com>
2016-08-04 14:48:55 +0200

    Added additionale project files via GitHub

---
+++ RedEye/src/main/scala/it/reti/spark/iot/App.scala
@@ -0,0 +1,26 @@
+package it.reti.spark.iot
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * @author ${user.name}
+ */
+object App {
+
+  def main(args : Array[String]) {
+    val conf = new SparkConf()
+      .setAppName("The swankiest Spark app ever")
+      .setMaster("local[*]")
+
+    val sc = new SparkContext(conf)
+
+    val col = sc.parallelize(0 to 100 by 5)
+    val smp = col.sample(true, 4)
+    val colCount = col.count
+    val smpCount = smp.count
+
+    println("orig count = " + colCount)
+    println("sampled count = " + smpCount)
+  }
+
+}
